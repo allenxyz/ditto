1139f2dd9376be4efdf641ecdce7d6fd230e4f69
patwan <patwan@0587352880.wireless.umich.net>
2014-11-23 17:05:03 -0500

    fixed some filter junk

--- src/eecs285/proj4/filters.java
+++ src/eecs285/proj4/filters.java
@@ -2,9 +2,11 @@ package eecs285.proj4;
 import eecs285.proj4.pixelTypes.*;
 import java.lang.Math.*;
 
-public class filters{
+public class filters
+{
 
-  void valencia(rgb val){
+  void valencia(rgb val)
+  {
     hsl hslLayer = rgb2hsl(val);
     hslLayer.changeL(hslLayer.getL() + 12);
     //should I do a soft light blend? I could :^) but it'd be hella hard
@@ -16,16 +18,26 @@ public class filters{
     hslLayer = rgb2hsl(rgbLayer);
     hslLayer.changeS(hslLayer.getS() - 20);
     //then somehow add noise, what the fuk
+    rgb temp = hsl2rgb(hslLayer);
+    val.changeR(temp.getR());
+    val.changeG(temp.getG());
+    val.changeB(temp.getB());
   }
   
-  void greyscale(rgb val){
+  void greyscale(rgb val)
+  {
     cmyk cmykLayer = rgb2cmyk(val);
     cmykLayer.changeC(0);
     cmykLayer.changeM(0);
     cmykLayer.changeY(0);
+    rgb temp = cmyk2rgb(cmykLayer);
+    val.changeR(temp.getR());
+    val.changeG(temp.getG());
+    val.changeB(temp.getB());
   }
   
-  public hsl rgb2hsl(rgb val){
+  public hsl rgb2hsl(rgb val)
+  {
     int rPrime = val.getR()/255;
     int gPrime = val.getG()/255;
     int bPrime = val.getB()/255;
@@ -37,27 +49,33 @@ public class filters{
     int saturation = 0;
     int lightness = (max + min)/2;
     
-    if(max == rPrime){
+    if(max == rPrime)
+    {
       hue = 60*((gPrime - bPrime)/delta) % 6;
     }
-    else if(max == gPrime){
+    else if(max == gPrime)
+    {
       hue = 60*((bPrime - rPrime)/delta + 2);
     }
-    else if(max == bPrime){
+    else if(max == bPrime)
+    {
       hue = 60*((rPrime - gPrime)/delta + 4); 
     }
     
-    if(delta == 0){
+    if(delta == 0)
+    {
       saturation = 0;
     }
-    else{
+    else
+    {
       saturation = delta/(1 - Math.abs(2 * lightness - 1));
     }
     
       return new hsl(hue, saturation, lightness);
     }
   
-  public rgb hsl2rgb(hsl val){
+  public rgb hsl2rgb(hsl val)
+  {
     int c = (1 - Math.abs(2 * val.getL())) * val.getS();
     int x = c * (1 - Math.abs((val.getH()/60) % 2 - 1));
     int m = val.getL() - c/2;
@@ -68,32 +86,38 @@ public class filters{
     
     int whatTheHBobby = val.getH();
     
-    if(whatTheHBobby >= 0 && whatTheHBobby < 60){
+    if(whatTheHBobby >= 0 && whatTheHBobby < 60)
+    {
       red = c + m;
       green = x + m;
       blue = m;
     }
-    else if(whatTheHBobby >= 60 && whatTheHBobby < 120){
+    else if(whatTheHBobby >= 60 && whatTheHBobby < 120)
+    {
       red = x + m;
       green = c + m;
       blue = m;
     }
-    else if(whatTheHBobby >= 120 && whatTheHBobby < 180){
+    else if(whatTheHBobby >= 120 && whatTheHBobby < 180)
+    {
       red = m;
       green = c + m;
       blue = x + m;
     }
-    else if(whatTheHBobby >= 180 && whatTheHBobby < 240){
+    else if(whatTheHBobby >= 180 && whatTheHBobby < 240)
+    {
       red = m;
       green = x + m;
       blue = c + m;
     }
-    else if(whatTheHBobby >= 240 && whatTheHBobby < 300){
+    else if(whatTheHBobby >= 240 && whatTheHBobby < 300)
+    {
       red = x + m;
       green = m;
       blue = c + m;
     }
-    else if(whatTheHBobby >= 300 && whatTheHBobby < 360){
+    else if(whatTheHBobby >= 300 && whatTheHBobby < 360)
+    {
       red = c + m;
       green = m;
       blue = x + m;
@@ -102,7 +126,8 @@ public class filters{
     return new rgb(red, green, blue);
   }
   
-  public cmyk rgb2cmyk(rgb val){
+  public cmyk rgb2cmyk(rgb val)
+  {
     int rPrime = val.getR()/255;
     int gPrime = val.getG()/255;
     int bPrime = val.getB()/255;
@@ -114,5 +139,20 @@ public class filters{
     int yellow = (1 - bPrime - key)/(1 - key);
     
     return new cmyk(cyan, magneta, yellow, key);
+  }
+
+
+  public rgb cmyk2rgb(cmyk val)
+  {
+    int c = val.getC();
+    int m = val.getM();
+    int y = val.getY();
+    int k = val.getK();
+    
+    int red = 255 * (1 - c) * (1 - k);
+    int green = 255 * (1 - m) * (1 - k);
+    int blue = 255 * (1 - y) * (1 - k);
+    
+    return new rgb(red, green, blue);
   }
 }
