c2b3f60e0c36e8d7e13051fd0cc8bfd5ce9347b5
Jessica Wu <jesswu@umich.edu>
2014-11-24 02:09:19 -0500

    was about to do custom filters

--- src/eecs285/proj4/ColorScheme.java
+++ src/eecs285/proj4/ColorScheme.java
@@ -6,24 +6,15 @@ public class ColorScheme
 {
   static Color colors[] = new Color[256];
   static int numbercolors;
-<<<<<<< HEAD
-
-=======
   static String name;
   
->>>>>>> e8c84c926034f8b436f348cd8591d2b9e5d45c27
   public ColorScheme()
   {
     // TODO Auto-generated constructor stub
   }
-<<<<<<< HEAD
 
-  public ColorScheme(Color colArr[], int numCols)
-  {
-=======
   
   public ColorScheme(String s, Color colArr[], int numCols){
->>>>>>> e8c84c926034f8b436f348cd8591d2b9e5d45c27
     System.arraycopy(colArr, 0, colors, 0, colArr.length);
     numbercolors = numCols;
     name = s;
--- src/eecs285/proj4/ImageProcessorGUI.java
+++ src/eecs285/proj4/ImageProcessorGUI.java
@@ -349,6 +349,7 @@ public class ImageProcessorGUI extends JFrame
     TitledBorder CustomFilter = new TitledBorder("Custom Filter");
     Custom.setBorder(CustomFilter);
     CustomFilters = new JComboBox<String>();
+    
     Custom.add(CustomFilters);
     // Custom.setVisible(false);
     // CustomFilters.addActionListener(new CustomFilter());
