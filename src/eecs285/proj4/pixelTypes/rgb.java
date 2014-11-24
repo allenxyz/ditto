c8fd9153185b80d2cc228be0233fde53cd1c4ae5
Allen Zhao <allenxyz@umich.edu>
2014-11-23 19:40:29 -0500

    not much done

--- src/eecs285/proj4/pixelTypes/rgb.java
+++ src/eecs285/proj4/pixelTypes/rgb.java
@@ -66,7 +66,7 @@ public class rgb{
   
 //  public static BufferedImage toImage(rgb[][] pixelVal)
 //  {
-////     
+//     
 ////     BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 ////     WritableRaster raster = (WritableRaster) image.getData();
 ////     raster.setPixels(0,0,width,height,pixels);
